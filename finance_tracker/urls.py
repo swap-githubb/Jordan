f571ad553ffa6fea9593f0d35a2612115901a367
swap-githubb <125266256+swap-githubb@users.noreply.github.com>
2025-04-18 02:15:49 +0530

    Update urls.py

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('apps.transactions.urls')),  # home/dashboard at root
 ]
 
